5b7432a53906402c2d219160294a9c1ace1ff826
AsyaDoga <49779152+AsyaDoga@users.noreply.github.com>
2019-04-19 16:03:08 +0300

    Add files via upload

---
+++ Pet&PlantCare/creature/PlantTask.java
@@ -0,0 +1,21 @@
+/**
+ * @author AsyaDoğaÖzer
+ * @date 19.04.2019
+ */
+public class PlantTask extends Task
+{
+   //properties
+   public static final int WATER           = 0;
+   public static final int FERTILIZATION   = 1; 
+   public static final int PRUNNING        = 2;
+   public static final int CUSTOM          = 3;
+   
+   //constructor
+   PlantTask()
+   {
+   }
+   
+   //methods
+   
+   
+}
