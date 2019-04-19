51b7150fb5313c6786a661888f519516993e9dd2
OnurOr <49712937+OnurOr@users.noreply.github.com>
2019-04-19 12:51:50 +0300

    Update Cat.java

--- Pet&PlantCare/creature/Cat.java
+++ Pet&PlantCare/creature/Cat.java
@@ -5,5 +5,12 @@ public class Cat extends Mammal
    public Cat()
    {
       super();
+      addTask( this, FOOD);
+      addTask( this, VACCINATION);
+      addTask( this, MEDICATION);
+      addTask( this, HYGIENE);
+      addTask( this, NAILS);
+      addTask( this, WATER_ANIMAL);
+      addTask( this, CUSTOM_ANIMAL);
    }
 }
