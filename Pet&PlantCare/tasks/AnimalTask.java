c547abcf9b64c44c7c7ffd8a957eda462b7e83b6
OnurOr <49712937+OnurOr@users.noreply.github.com>
2019-04-19 12:44:41 +0300

    Add files via upload

---
+++ Pet&PlantCare/tasks/AnimalTask.java
@@ -0,0 +1,30 @@
+
+
+/**
+ * creates tasks related to animals
+ * @author Onur Oruç
+ * @date 19.04.2019
+ */
+
+public class AnimalTask {
+   
+   //properties
+   public static final int CUSTOM_ANIMAL  = -1;
+   public static final int FOOD           = 0;
+   public static final int VACCINATION    = 1;
+   public static final int MEDICATION     = 2;
+   public static final int HYGIENE        = 3;
+   public static final int NAILS          = 4;
+   public static final int WATER_ANIMAL   = 5;
+   
+   
+   //constructor
+   
+   
+   //methods
+
+}
+
+   
+   
+      
