8e27c08c04c940092ef87c3ab1c5d6ae615c6b65
OnurOr <49712937+OnurOr@users.noreply.github.com>
2019-04-19 12:54:11 +0300

    Update Dog.java

--- Pet&PlantCare/creature/Dog.java
+++ Pet&PlantCare/creature/Dog.java
@@ -5,5 +5,12 @@ public class Dog extends Mammal
    public Dog()
    {
       super();
+      addTask( this, AnimalTask.FOOD);
+      addTask( this, AnimalTask.VACCINATION);
+      addTask( this, AnimalTask.MEDICATION);
+      addTask( this, AnimalTask.HYGIENE);
+      addTask( this, AnimalTask.NAILS);
+      addTask( this, AnimalTask.WATER_ANIMAL);
+      addTask( this, AnimalTask.CUSTOM_ANIMAL);
    }
 }
