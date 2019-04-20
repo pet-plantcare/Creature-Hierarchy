618ab68ba16cc3b01a27d44df6a1038dffc5e611
OnurOr <49712937+OnurOr@users.noreply.github.com>
2019-04-20 21:51:41 +0300

    Update Fish.java

--- Pet&PlantCare/creature/Fish.java
+++ Pet&PlantCare/creature/Fish.java
@@ -16,7 +16,6 @@ public class Fish  extends Animal
       //for costomized tasks
       addTask( AnimalTask.CUSTOM_ANIMAL);
       
-      fishes.add( this);
    }
    
    public void setSpecies( String species)
