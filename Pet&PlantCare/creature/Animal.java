d934b55ff06790d6672d856f0a82b54aea382a62
UmutAdaYuruten <47862628+UmutAdaYuruten@users.noreply.github.com>
2019-04-23 15:40:55 +0300

    Update Animal.java

--- Pet&PlantCare/creature/Animal.java
+++ Pet&PlantCare/creature/Animal.java
@@ -19,15 +19,21 @@ public abstract class Animal extends Creature
       fishes = new ArrayList<Fish>();
       birds = new ArrayList<Bird>();    
    }
-   
-   //methods
-   @Override
-   /**
-    * adds new tasks to animal.
-    * @param taskType, sign of the task that will be added.
-    */ 
-   public void addTask( int taskType) { //INSTANTIATING TO ANIMALTASK SINCE ABSTRACT CLASSES CAN NOT BE INSTANTIATED!!
-      Task newTask = new AnimalTask( this, taskType);
-      tasks.add( newTask);
+
+      //methods
+      @Override
+      /**
+       * adds new tasks to animal.
+       * @param taskType, sign of the task that will be added.
+       */ 
+      public void addTask( int taskType) { //INSTANTIATING TO ANIMALTASK SINCE ABSTRACT CLASSES CAN NOT BE INSTANTIATED!!
+         Task newTask = new AnimalTask( this, taskType);
+         tasks.add( newTask);
+      }
+      
+      public void createCustomTask()
+      {
+         //for costomized tasks
+            addTask( AnimalTask.CUSTOM_ANIMAL);
+      }
    }
-}
