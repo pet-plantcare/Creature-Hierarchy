648b0759ddb4a32e0f5e5e33ea63bc005fc202fc
UmutAdaYuruten <47862628+UmutAdaYuruten@users.noreply.github.com>
2019-04-23 15:45:05 +0300

    Update Bird.java

--- Pet&PlantCare/creature/Bird.java
+++ Pet&PlantCare/creature/Bird.java
@@ -14,15 +14,14 @@ public class Bird extends Animal
    {
       super();
       gender = "N/A";
-      addTask(this, AnimalTask.FOOD);
-      addTask(this, AnimalTask.VACCINATION);
-      addTask(this, AnimalTask.MEDICATION);
-      addTask( this,AnimalTask.HYGIENE);
-      addTask( this,AnimalTask.NAILS);
-      addTask( this,AnimalTask.WATER_ANIMAL);
+      addTask( AnimalTask.FOOD);
+      addTask( AnimalTask.VACCINATION);
+      addTask( AnimalTask.MEDICATION);
+      addTask( AnimalTask.HYGIENE);
+      addTask( AnimalTask.NAILS);
+      addTask( AnimalTask.WATER_ANIMAL);
       
-      //for customized tasks
-      addTask( this,AnimalTask.CUSTOM_ANIMAL);
+
    }
    
    public void setGender(String gender)
