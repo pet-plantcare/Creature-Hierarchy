bbce9707a9ebe43897dd21800af3d79564f4caa6
UmutAdaYuruten <47862628+UmutAdaYuruten@users.noreply.github.com>
2019-05-09 01:26:45 +0300

    Create plant_profile_activity.java

---
+++ plant_profile_activity.java
@@ -0,0 +1,59 @@
+package com.example.navigationalbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+import android.widget.Button;
+import android.view.View;
+//import androidx.appcompat.app.AppCompatActivity;
+
+public class plant_profile_activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+
+        Button addTask = (Button) findViewById(R.id.addTask_button);
+
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(profile_page_activity,addTask_page.class);
+                //startActivity(intent);
+            }
+        });
+
+        Button gallery = (Button) findViewById(R.id.gallery_button2);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(profile_page_activity.this,);
+                //startActivity(intent);
+            }
+        });
+
+        Button history = (Button) findViewById(R.id.history_button);
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(profile_page_activity.this,);
+                //startActivity(intent);
+
+            }
+        });
+
+        Button edit = (Button) findViewById(R.id.edit_button);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(profile_page_activity.this,);
+                //startActivity(intent);
+
+            }
+        });
+
+    }
+}
