af9966b2358af26ef50bffa2cf1da5ba60cdc8d5
OnurOr <49712937+OnurOr@users.noreply.github.com>
2019-05-10 10:56:21 +0300

    Create HomeActivity.java

---
+++ HomeActivity.java
@@ -0,0 +1,39 @@
+package com.example.instagramclone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.instagramclone.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class HomeActivity extends AppCompatActivity {
+    private static final int ACTIVITY_NUM = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        setupBottomNavigationView();
+
+
+    }
+
+
+    private void setupBottomNavigationView() {
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setUpBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(HomeActivity.this, bottomNavigationViewEx);// bu her yeni açılan classta eklenecek "HomeActivity.this" kısmı o class ismiyle değişecek"
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+
+    }
+}
