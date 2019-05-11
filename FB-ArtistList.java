61a895c300dd06625bd39cd7058be01fdfd5c2d2
gamzeguliter <49712771+gamzeguliter@users.noreply.github.com>
2019-05-11 13:16:44 +0300

    Create FB-ArtistList.java

---
+++ FB-ArtistList.java
@@ -0,0 +1,39 @@
+package com.example.fb;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ArtistList extends ArrayAdapter<Artist> {
+
+    private Activity context;
+    private List<Artist> artistList;
+
+    public ArtistList(Activity context, List<Artist> artistList) {
+        super(context,R.layout.list_layout,artistList);
+        this.context = context;
+        this.artistList = artistList;
+    }
+
+    @Override
+    public View getView(int position,View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewİtem =inflater.inflate(R.layout.list_layout,null,true);
+
+        TextView textViewName = (TextView) listViewİtem.findViewById(R.id.textViewName);
+        TextView textViewGenre = (TextView) listViewİtem.findViewById(R.id.textViewGenre);
+
+        Artist artist = artistList.get(position);
+
+        textViewName.setText(artist.getArtistName());
+        textViewGenre.setText(artist.getArtistGenre());
+
+        return listViewİtem;
+    }
+}
